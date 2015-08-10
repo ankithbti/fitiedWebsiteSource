95349378f1f4d21fa3a943a8a8f18a39b9d6ddd0
Ankit Gupta <ankithbti007@gmail.com>
2015-08-10 23:38:56 +0530

    Initial Commit.

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	include 'pageHeader.php';
+?>
+
+<body>
+
+<div class="container">
+	<h1>Fitied</h1>
+</div>
+
+</body>
+
+
+<?php
+	include 'pageFooter.php';
+?>
---
+++ pageHeader.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fitied</title>
+
+	<!-- Include Bootstrap CSS File -->
+	<link rel="stylesheet" type="text/css" href="third_party/bootstrap-3.3.5-dist/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="third_party/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/common.css" />
+</head>
